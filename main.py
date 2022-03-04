beae5be25548e7071006b7b81f41802868fbbacb
Zack Balbin <zack@zackbalbin.com>
2022-03-03 19:24:40 -0800

    update script to current on aws lambda

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
-import requests
-import sys
+import json
+import urllib3
 
-ERGO_TESTNET_EXPLORER_API_BASEURL = "https://api-testnet.ergoplatform.com"
 ERGO_MAINNET_EXPLORER_API_BASEURL = "https://api.ergoplatform.com"
+ERGO_TESTNET_EXPLORER_API_BASEURL = "https://api-testnet.ergoplatform.com"
 
 def create_url(transactionId, networkType):
+    restOfUrl = "/api/v1/transactions/" + transactionId
     if networkType == "MAINNET":
-        url = ERGO_MAINNET_EXPLORER_API_BASEURL + "/api/v1/transactions/" + transactionId
+        url = ERGO_MAINNET_EXPLORER_API_BASEURL + restOfUrl
     elif networkType == "TESTNET":
-        url = ERGO_TESTNET_EXPLORER_API_BASEURL + "/api/v1/transactions/" + transactionId
+        url = ERGO_TESTNET_EXPLORER_API_BASEURL + restOfUrl
     return url
 
-def get_transaction(transactionId, networkType):
+def get_transaction_data(transactionId, networkType):
+    http = urllib3.PoolManager()
     url = create_url(transactionId, networkType)
-    response = requests.get(url)
-    data = response.json()
-    return data
+    response = http.request('GET', url)
+    transactionData = response.data
+    return transactionData
 
-def get_transaction_blockid(transaction):
-    blockid = transaction['blockId']
+def get_transaction_blockid(transactionData):
+    jsonData = convert_http_response_to_json(transactionData)
+    blockid = jsonData['blockId']
     return blockid
-
-def main():
-
-    transactionid = str(sys.argv[1])
-    networkType = str(sys.argv[2])
-    transaction = get_transaction(transactionid, networkType)
     
-    blockid = get_transaction_blockid(transaction)
+def convert_http_response_to_json(responseData):
+    jsonData = json.loads(responseData)
+    return jsonData
 
-    print(blockid)
-
-if __name__=="__main__":
-    main()
+def lambda_handler(event, context):
+    transactionId = event.get("transactionId")
+    networkType = event.get("networkType")
+    transaction = get_transaction_data(transactionId, networkType)
+    blockId = get_transaction_blockid(transaction)
+    return {
+        'statusCode': 200,
+        'body': blockId
+    }
