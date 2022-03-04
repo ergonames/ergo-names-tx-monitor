749ddf678eeba8e6afc4a88c9f53ffcabf608af8
Zack Balbin <zack@zackbalbin.com>
2022-03-03 23:53:59 -0800

    update to access sqs queue

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
 import urllib3
+import boto3
 
 ERGO_MAINNET_EXPLORER_API_BASEURL = "https://api.ergoplatform.com"
 ERGO_TESTNET_EXPLORER_API_BASEURL = "https://api-testnet.ergoplatform.com"
+
+SQS_QUEUE_URL = ""
 
 def create_url(transactionId, networkType):
     restOfUrl = "/api/v1/transactions/" + transactionId
@@ -33,6 +36,29 @@ def lambda_handler(event, context):
     networkType = event.get("networkType")
     transaction = get_transaction_data(transactionId, networkType)
     blockId = get_transaction_blockid(transaction)
+    
+    sqs = boto3.client('sqs')
+    response = sqs.send_message(
+        QueueUrl=SQS_QUEUE_URL,
+        DelaySeconds=0,
+        MessageAttributes={
+            'transactionId': {
+                'DataType': 'String',
+                'StringValue': transactionId
+            },
+            'networkType': {
+                'DataType': 'String',
+                'StringValue': networkType
+            },
+            'blockId': {
+                'DataType': 'String',
+                'StringValue': blockId
+            }
+        },
+        MessageBody=(
+            "Transaction Information with BlockId"
+        )
+    )
     return {
         'statusCode': 200,
         'body': blockId
